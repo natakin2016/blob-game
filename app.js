fd01f3236e9a63cd8f27860168ca875f9e1772d1
TheLinuxNerd <huckabywyatt@gmail.com>
2016-07-15 21:45:06 -0700

    added basic health mechanic, sorry for lack of active coding, been kinda busy, will make all the java one night, when i dont have to wake up at 6

--- app.js
+++ app.js
@@ -1,3 +1,13 @@
 var MonsterHealth = 0;
-var UserLevel = 0;
+var UserLevel = 1;
+
+//document.getElementByID("test-level").innerHTML = "User Level: " + UserLevel;
+//document.getElementByID("test-monsterhp").innerHTML = "Monster Health " + MonsterHealth;
+
+function applyLevel(){
+	MonsterHealth = (Math.pow(2 * UserLevel, 2)) * 5;
+	console.log("User Level: " + UserLevel + "|" + "Monster Health: " + MonsterHealth);
+}
+
+
 
